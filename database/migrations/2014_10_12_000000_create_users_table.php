e2c9de4dd692baa44b63975d042035d5f18ff074
Dyann2002 <lopk1162002@gmail.com>
2023-12-13 17:02:59 +0700

    [Chapter_3] commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->text('password');
+            $table->string('email', 320)->unique();
+            $table->integer('role_id')->default(1);
+            $table->integer('status_id')->default(1);
+            $table->boolean('verify')->default(false);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar', 255)->nullable();
+            $table->tinyInteger('gender')->nullable();
+            $table->date('birthday')->nullable();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
